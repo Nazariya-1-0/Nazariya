c1abf4d0cdcb99a8f8fc4341fcea74b66cf9c239
Ayush-Baliyan-19 <ayush.3316403221@ipu.ac.in>
2023-06-02 00:21:31 +0530

    Added tail and router

---
+++ src/Components/HeroNav.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HeroNav = () => {
+  return (
+    <div>
+        <p>Good Morning</p>
+        <p className='text-2xl font-bold'>Batricia Salfiora</p>
+        <div className="butttton"></div>
+        </div>
+  )
+}
+
+export default HeroNav
