7429591a8c7aa702357c0cb217b3dd770052d651
Ayush-Baliyan-19 <ayush.3316403221@ipu.ac.in>
2023-06-02 07:02:35 +0530

    added notes

--- src/Components/Detail.js
+++ src/Components/Detail.js
@@ -21,7 +21,7 @@ const Detail = () => {
             </Link>
           </div>
           <div className="mid flex flex-col justify-center items-center">
-            <img src={require("./assets/images/rich_dad_poor_dad.jpeg")} alt="" srcset="" />
+            <img src={require("./assets/images/rich_dad_poor_dad.jpeg")} className='w-38 h-52' alt="" srcset="" />
             <p className='txtCol text-lg font-bold'>The Tiny Dragon</p>
             <p className='txtCol font-semibold'>Rupret Carter</p>
           </div>
@@ -51,7 +51,7 @@ const Detail = () => {
             <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5V2zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1H4z" />
           </svg>
         </Link>
-        <Link className='read txtCol'>Start Reading</Link>
+        <Link className='read txtCol' to="/detail/rich_dad/notes">Start Reading Notes</Link>
       </div>
     </div>
   )
